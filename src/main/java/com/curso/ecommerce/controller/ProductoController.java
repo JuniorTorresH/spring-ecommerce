3dfba1483b44d4a66dfdac6d6d52143ffb0879c1
Junior Torres <jtorresh.92@gmail.com>
2024-07-02 18:52:09 -0500

    funcionalidad para eliminar un registro producto

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -67,6 +67,15 @@ public class ProductoController {
 		productoService.update(producto);
 		return "redirect:/productos";
 	}
+	
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable Integer id) {
+		productoService.delete(id);
+		return "redirect:/productos";
+	}
+	
+	
+	
 }
 
 
